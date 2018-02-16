1206c11a447740d790d03fb7bec8b9e8cb795fc2
Tiffany Moeller <tmoellerart@gmail.com>
2018-02-16 17:27:17 -0600

    start nltk features

--- feature_creation.py
+++ feature_creation.py
@@ -4,24 +4,27 @@ from db_functions import db_create_engine
 from textblob import TextBlob
 import re
 from nltk.corpus import stopwords
-stopwords.words('english')
+from nltk.stem import WordNetLemmatizer
+import nltk
+from collections import Counter
+
+wordnet_lemma = WordNetLemmatizer()
 
 
-engine = db_create_engine(config_file='config.ini',
-                          conn_name='PostgresConfig')
-
-all_tweets = pd.read_sql_query(sql=tweets_sql, con=engine)
-
-
-def clean_tweet(tweet):
+def remove_urls_punct(tweet):
     """
     Utility function to clean tweet text by removing links, special characters
     using simple regex statements.
     :param tweet:
     :return:
     """
+    # 1. Remove all urls
     remove_urls = re.sub(r"http\S+", "", tweet)
-    return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])| (\w+:\ / \ / \S+)", " ", remove_urls).split())
+
+    # 2. Remove all punctuation
+    clean = re.sub(r'[^\w\s]', '', remove_urls)
+
+    return clean
 
 
 def get_tweet_sentiment(tweet):
@@ -29,7 +32,7 @@ def get_tweet_sentiment(tweet):
     Utility function to classify sentiment of passed tweet
     using textblob's sentiment method
     """
-    analysis = TextBlob(clean_tweet(tweet))
+    analysis = TextBlob(remove_urls_punct(tweet))
 
     if analysis.sentiment.polarity > 0:
         return 1
@@ -37,11 +40,6 @@ def get_tweet_sentiment(tweet):
         return 0
     else:
         return -1
-
-
-def get_tweet_normalized(tweet, stemmer):
-    text = [t for t in clean_tweet(tweet).split() if t not in stopwords.words('english')]
-    return [stemmer.stem(t) for t in text]
 
 
 def find_rate_all_caps(tweet):
@@ -85,8 +83,98 @@ def generate_features(df):
     return df[relevant_cols], df
 
 
-features, all_df = generate_features(all_tweets)
+def fetch_all_tweets(config_file, conn_name):
+    """
+    Utility function to fetch tweet data from Postgres
+    :param config_file: commonly 'config.ini' - file where config details are stored
+    :param conn_name: section in config file with db connection and config details
+    :return: pandas dataframe with all tweets
+    """
+    engine = db_create_engine(config_file=config_file,
+                              conn_name=conn_name)
+
+    all_tweets = pd.read_sql_query(sql=tweets_sql, con=engine)
+
+    return all_tweets
 
 
-features.to_pickle('data/feature_engineering.pkl')
-all_df.to_csv('data/all_feature_engineering.csv')
+def pickle_base_features(tweets):
+    """
+    Utility function to transform and pickle raw tweet data as feature/target matrix
+    :param tweets:
+    :return:
+    """
+    features, all_df = generate_features(tweets)
+
+    features.to_pickle('data/feature_engineering.pkl')
+    all_df.to_csv('data/all_feature_engineering.csv')
+
+
+def all_elements(column):
+    """
+    Takes a column containing a row-wise list of elements, removes any braces and splits each element
+    out into a list of total elements
+    """
+    elements_list = []
+
+    for row in column:
+        clean = row.replace('{', '').replace('}', '')
+
+        for element in clean.split(','):
+            if element != '':
+                elements_list.append(element.lower())
+
+    return elements_list
+
+
+def find_most_common_elements(list_elements, find_top_x):
+    """
+    Takes in list of elements and returns the top x most common
+    """
+    count_elements = Counter(list_elements)
+    return count_elements.most_common(find_top_x)
+
+
+def clean_tweets(tweets):
+    cleaned_tweets = []
+
+    for i in tweets:
+
+        # 1. Remove all urls and punctuation
+        clean = remove_urls_punct(i)
+
+        # 2. Convert to string
+        uni = [str(i) for i in clean.lower().split()]
+
+        # 3. Remove english stop words
+        stops = set(stopwords.words("english"))
+        meaningful_words = [w for w in uni if not w in stops]
+
+        # 4. Create lemmas for meaningful words
+        lemmas = [wordnet_lemma.lemmatize(i) for i in meaningful_words]
+
+        cleaned_tw = " ".join( lemmas )
+        cleaned_tweets.append(cleaned_tw)
+
+    return cleaned_tweets
+
+
+def tokenize_tweets(tweets):
+    tweet_tokens = []
+
+    for i in clean_tweets(tweets):
+
+        tokens = nltk.word_tokenize(i, "english")
+        tweet_tokens.append(tokens)
+
+    return tweet_tokens
+
+
+# Find Features
+def find_features(job, feature_set):
+    features = {}
+    for w in feature_set:
+        features[w] = (w in job)
+
+    return features
+
